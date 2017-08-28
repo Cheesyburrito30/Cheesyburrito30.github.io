124ca043f816925036d525fbb82df625974efebc
Vernon Perry <pvernon30@gmail.com>
2017-08-28 14:14:59 -0400

    initial commit

---
+++ public/JS/main.js
@@ -0,0 +1,16 @@
+$(function(){
+
+	$("#Github").mouseover(function(){
+		$(this).attr('src', '/images/GitHub-Mark-Light-32px.png')
+	})
+	$("#Github").mouseleave(function(){
+		$(this).attr('src', '/images/GitHub-Mark-32px.png')
+	})
+
+	$("#linkedIn").mouseover(function(){
+		$(this).attr('src', 'images/social-linkedin-box-white-icon.png')
+	})
+	$("#linkedIn").mouseleave(function(){
+		$(this).attr('src', '/images/linkedin-logo.svg')
+	})
+})
