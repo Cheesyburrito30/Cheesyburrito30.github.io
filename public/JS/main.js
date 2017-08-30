c866c9ea6f4a304bcad92bf98ca390a4ad3de997
Vernon Perry <pvernon30@gmail.com>
2017-08-30 07:16:08 -0400

    image links

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,16 +1,16 @@
 $(function(){
 
 	$("#Github").mouseover(function(){
-		$(this).attr('src', '/images/GitHub-Mark-Light-32px.png')
+		$(this).attr('src', 'images/GitHub-Mark-Light-32px.png')
 	})
 	$("#Github").mouseleave(function(){
-		$(this).attr('src', '/images/GitHub-Mark-32px.png')
+		$(this).attr('src', 'images/GitHub-Mark-32px.png')
 	})
 
 	$("#linkedIn").mouseover(function(){
-		$(this).attr('src', '/images/social-linkedin-box-white-icon.png')
+		$(this).attr('src', 'images/social-linkedin-box-white-icon.png')
 	})
 	$("#linkedIn").mouseleave(function(){
-		$(this).attr('src', '/images/linkedin-logo.svg')
+		$(this).attr('src', 'images/linkedin-logo.svg')
 	})
 })
